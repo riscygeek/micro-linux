6e5e384d81416d2890939dfe030b8af615c8339d
Benjamin Stürz <benni@stuerz.xyz>
2022-01-06 17:13:57 +0100

    Clean start
    Signed-off-by: Benjamin Stürz <benni@stuerz.xyz>

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-qemu-system-x86_64               \
-   -accel kvm -smp cores=4 -m 8G \
-   -kernel rootfs/boot/vmlinuz   \
-   -hda rootfs.ext2              \
-   -nographic                    \
-   -append "earlyprintk console=ttyS0 root=/dev/sda"
-
